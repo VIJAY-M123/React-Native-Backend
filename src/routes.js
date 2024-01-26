9ede540b86a3d1802ec28a16b71ef6175fbd2b2b
Vijaya Raj <vijayaraj@aggrandizeventure.com>
2024-01-26 20:41:13 +0530

    Correction

--- src/routes.js
+++ src/routes.js
@@ -14,19 +14,22 @@ router.post("/login", async (req,res)=>{
   const user = await UserModel.findOne({email:email})
 
   if(!user){
-    return res.json({Message:"User Not Fount Please Register"})
+    return res.status(201).json({message:"User Not Fount Please Register"})
   }
 
-  if(bcrypt.compare(password, user.password)){
+  const bcryptPassword = await bcrypt.compare(password, user.password)
+  
+
+  if(bcryptPassword){
     const token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: '1h' });
-    if(res.status(201)){
-      return res.json({message:"User Login sucessfully", token:token})
+    if(res.status(200)){
+      return res.status(200).json({message:"User Login sucessfully", token:token})
     }
     else{
-      return res.json({message:"User Login Error"})
+      return res.status(201).json({message:"User Login Error"})
     }
   }
-  res.json({message:'Invalid Password'})
+  res.status(201).json({message:'Invalid Password'})
 })
 
 router.post("/register", async (req,res)=>{
@@ -35,7 +38,7 @@ const oldUser = await UserModel.findOne({email:email})
 const hashpassowrd = await bcrypt.hash(password,10)
 
 if(oldUser){
-     return res.send({status:'error', data:"User already register"})
+     return res.json({ message:"User already register Please Login!!"})
  }
 
  try{
@@ -48,7 +51,7 @@ if(oldUser){
     res.status(200).json({message:"User register Successfully", username:username})
  }
  catch(err){
-  res.status(500).json({message:"User register error"})
+  res.json({message:"User register error"})
  }
 
 })
